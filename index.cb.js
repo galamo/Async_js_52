99fccb7a29e627ad431aaf06a8d56100d9cbc06a
galamo <galamouyal88@gmail.com>
2020-07-13 20:12:19 +0300

    feat-async-js

--- index.cb.js
+++ index.cb.js
@@ -1,21 +1,26 @@
 
 
-function getProductsFromServer () {
-    return products
+function getProductsFromServer (callback) {
+    setTimeout(() => {
+        callback(products)
+    }, 5000)
 }
 
+// function () { ================ callback
+// } ================ callback
 
 function init () {
     const container = $("#productsContainer")
-    const products = getProductsFromServer()
-
-    const listItems = products.map((p) => {
-        return getListItem(p)
+    getProductsFromServer(function (products) {
+        const listItems = products.map((product) => {
+            return getListItem(product)
+        })
+        container.append(...listItems)
     })
-    container.append(...listItems)
+
     function getListItem (productData) {
         const { item, price } = productData
-        const listItem = document.createElement("li")
+        const listItem = document.createElement("li") //use jquery
         listItem.classList.add("list-group-item")
         listItem.innerText = `${item}, ${price}`
         return listItem
@@ -26,3 +31,27 @@ init()
 
 
 
+// function hi (param) {
+//     param()
+// }
+
+// hi(() => {
+//     console.log("i am smart")
+// })
+
+
+
+
+
+
+
+
+
+// const a = function (products) {
+//     const listItems = products.map((p) => {
+//         return getListItem(p)
+//     })
+//     container.append(...listItems)
+// }
+
+// a()
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Callback
+- create your own callback, create search operation with input + button
+- the result from the search will take at least 2 sec
