f9ad221fc1fbf935d41ce7c128f47a3bc734a733
galamo <galamouyal88@gmail.com>
2020-07-16 21:46:30 +0300

    get_country_by_language

--- countries/index.js
+++ countries/index.js
@@ -4,6 +4,7 @@ async function init () {
     try {
         const result = await getCountries({ url: "https://restcountries.eu/rest/v2/all" })
         const countriesByLanguages = getCountryByLanguage(result, $("#langValue").val())
+        $("#container-data").text(JSON.stringify(result))
         if (!countriesByLanguages) return
         $("#container-data").text(JSON.stringify(countriesByLanguages))
     } catch (err) {
@@ -14,15 +15,25 @@ async function init () {
 (function () {
     $("#GET_COUNTRIES").on("click", init)
 
-
-
 })()
 
 function getCountryByLanguage (countries, language) {
     if (!language) return
-    alert(language)
+    const requestedLanguage = language.toLowerCase()
+    const result = countries.map(country => country.languages)
 
+    const final = countries.reduce((countryObj, country) => {
+        const { languages } = country
+        const languagesNames = languages.map(languageObj => languageObj.name)
+        const isLanguageExistInCountry = languagesNames.some(langString => langString.toLowerCase().includes(requestedLanguage))
+        if (isLanguageExistInCountry) {
+            return { ...countryObj, [ country.name ]: country }
+        } else {
+            return { ...countryObj }
+        }
+    }, {})
 
+    console.log(final)
 }
 
 // init()
